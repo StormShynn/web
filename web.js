1181920988cde5a790de9a4ceff43b25d0357500
StormShyn <chinghia19031999@gmail.com>
2022-08-05 09:26:15 +0700

    Update web.js

--- web.js
+++ web.js
@@ -1,13 +1,1 @@
-<!DOCTYPE html>
-<html>
-<body>
-    <p id="demo1"></p>
-    <p id="demo2"></p>
-    <script>
-     var str = "Website Hay";
-     var result_1 = "http://zevolving.com/".link("http://zevolving.com/");
-     document.getElementById("demo1").innerHTML = result_1;
-     document.getElementById("demo2").innerHTML = result_2;
-    </script>
-</body>
-</html>
+http://zevolving.com/
